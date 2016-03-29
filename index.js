ee7b0126fab0e8f02ac704bd700ea5a1e551c0a1
Geoff Doty <n2geoff@gmail.com>
2016-03-29 18:59:56 -0400

    adjusting index for testing

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Hapi = require("hapi");
 
 const server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({ port: 8000 });
 
 server.register(require("./config/plugins"), function (err) {
     if (err) { return console.log(err); }
+    
     // register routes
     server.route(require('./routes/public'));
     server.route(require('./routes/users'));
@@ -14,3 +15,5 @@ server.register(require("./config/plugins"), function (err) {
         console.log('API up and running at:', server.info.uri);
     });
 });
+
+module.exports = server;
